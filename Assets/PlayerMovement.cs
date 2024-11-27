fe68d4f215fc491e62ec2f174766d91bdd967469
AidenJWright <96337835+AidenJWright@users.noreply.github.com>
2024-11-26 17:15:04 -0800

    mygame

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    int dirx;
+    int diry;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirx = 0;
+        diry = 0;
+        if (Input.GetKey(KeyCode.D))
+        {
+            dirx++;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            dirx--;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            diry++;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            diry--;
+        }
+        Vector3 pos = transform.position;
+        transform.position = new Vector3(pos.x + dirx * speed * Time.deltaTime, pos.y + diry * speed * Time.deltaTime , pos.z);
+
+
+
+    }
+}
+
+
+     
